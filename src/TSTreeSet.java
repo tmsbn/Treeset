b8cc341dd2de59fd559f18cce8bd14379d17bb99
Savitha <savi.kj@gmail.com>
2016-12-03 23:01:46 -0500

    basic hash for contains

--- src/TSTreeSet.java
+++ src/TSTreeSet.java
@@ -10,13 +10,16 @@ public class TSTreeSet<E extends Comparable<E>> extends TreeSet<E> {
 
     private Node<E> root;
     int size = 0;
-
+    Node hashTable[];
     boolean isElementFound = true;
+    final int MAX_ELEMENTS = 300000;
 
-    public TSTreeSet() {
+    public <E> TSTreeSet() {
+        hashTable = new Node[MAX_ELEMENTS];
     }
 
     public void init() {
+
     }
 
     /**
@@ -24,47 +27,50 @@ public class TSTreeSet<E extends Comparable<E>> extends TreeSet<E> {
      */
     @Override
     public boolean add(E e) {
+        Node<E> node = new Node<>(e);
 
-        if (!contains(e)) {
-            if (root == null) {
-                root = new Node<E>(e);
-                size++;
-                return true;
-            } else {
 
-                Node<E> parent;
-                Node<E> currentNode = root;
+        if (root == null) {
+            root = node;
+            hashTable[(Integer.parseInt((String) e))] = node;
+            size++;
+            return true;
+        } else {
 
-                while (true) {
+            Node<E> parent;
+            Node<E> currentNode = root;
 
-                    parent = currentNode;
-                    if (currentNode.e.compareTo(e) > 0) {
+            while (true) {
 
-                        currentNode = currentNode.left;
-                        if (currentNode == null) {
-                            parent.left = new Node<E>(e);
-                            parent.left.parent = parent;
-                            size++;
-                            return true;
-                        }
+                parent = currentNode;
+                if (currentNode.e.compareTo(e) > 0) {
 
-                    } else if (currentNode.e.compareTo(e) < 0) {
-
-                        currentNode = currentNode.right;
-                        if (currentNode == null) {
-                            parent.right = new Node<E>(e);
-                            parent.right.parent = parent;
-                            size++;
-                            return true;
-                        }
-                    } else if (currentNode.e.compareTo(e) == 0) {
-                        return false;
+                    currentNode = currentNode.left;
+                    if (currentNode == null) {
+                        parent.left = node;
+                        hashTable[(Integer.parseInt((String) e))] = node;
+                        parent.left.parent = parent;
+                        size++;
+                        return true;
                     }
 
+                } else if (currentNode.e.compareTo(e) < 0) {
+
+                    currentNode = currentNode.right;
+                    if (currentNode == null) {
+                        parent.right = node;
+                        hashTable[(Integer.parseInt((String) e))] = node;
+                        parent.right.parent = parent;
+                        size++;
+                        return true;
+                    }
+                } else if (currentNode.e.compareTo(e) == 0) {
+                    return false;
                 }
+
             }
-        } else
-            return false;
+        }
+       
     }
 
     public boolean remove(E e) {
@@ -193,20 +199,26 @@ public class TSTreeSet<E extends Comparable<E>> extends TreeSet<E> {
 
     @Override
     public boolean contains(Object o) {
-
+        Node nodes = hashTable[Math.abs(Integer.parseInt((String) o))];
         Comparable comparable = (Comparable) o;
-        Node currentNode = root;
-        while (currentNode != null) {
-            if (comparable.compareTo(currentNode.e) > 0) {
-                currentNode = currentNode.right;
-            } else if (comparable.compareTo(currentNode.e) < 0) {
-                currentNode = currentNode.left;
-            } else {
-                return true;
-            }
-        }
+        if (comparable.compareTo(nodes.e) == 0)
+            return true;
+        else
+            return false;
 
-        return false;
+//        Comparable comparable = (Comparable) o;
+//        Node currentNode = root;
+//        while (currentNode != null) {
+//            if (comparable.compareTo(currentNode.e) > 0) {
+//                currentNode = currentNode.right;
+//            } else if (comparable.compareTo(currentNode.e) < 0) {
+//                currentNode = currentNode.left;
+//            } else {
+//                return true;
+//            }
+//        }
+//
+//        return false;
     }
 
     public boolean isEmpty() {
